11c1c476064794318124111f39f2c36e766342e3
Ryan Dunn <me@ryandunn.co>
2018-07-05 12:13:47 -0500

    Added various files and configed them to work with remote repo

---
+++ config-example.php
@@ -0,0 +1,21 @@
+<?php
+// rename this file to "config.php" and update the "$authToken" & "$actionitem_tag_guid" 
+// configure the application 
+
+// toggle this variable to move between "Sandbox" and "Live"
+$dev_mode = false;
+
+if($dev_mode){
+    // place the auth token from your sandbox acccount here
+    $authToken = "...";
+
+    // "Action Item" tag GUID as it relates to your sandbox account here
+    $actionitem_tag_guid = array("...");
+}else{
+    // place the auth token from your live acccount here
+    $authToken = "...";
+
+    // "Action Item" tag GUID as it relates to your live account here
+    $actionitem_tag_guid = array("42cdbc60-d081-45b5-960e-1c7d0f78b066");
+}
+?>
